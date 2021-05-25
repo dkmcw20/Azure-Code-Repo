0bad9c91be07dd68403a18dfa3ff06b4214ee5cc
HungryBoySL <dkmcw20@users.noreply.github.com>
2021-05-25 23:45:08 +1000

    V2

---
+++ Create a new Disk from Azure VM Data Disk.ps1
@@ -0,0 +1,18 @@
+﻿$resourceGroup = "AUSE-DEV-KIWI-RG"
+$DiskName = "ODC1AP0027"
+$NewDiskName = "ODC1AP0027_OSDISK"
+$SubName = "AZPCM29421001-M-Opal"
+
+Connect-AzAccount
+
+#! Get source OS Disk information
+Write-Verbose "Get the source OS Disk information: $($VM.StorageProfile.OsDisk.Name)"
+$sourceOSDisk = Get-AzDisk -ResourceGroupName $resourceGroup -DiskName $DiskName
+
+#! Create the managed disk configuration
+Write-Verbose "Create the managed disk configuration..."
+$diskConfig = New-AzDiskConfig -SkuName $sourceOSDisk.Sku.Name -Location $sourceOSDisk.Location -DiskSizeGB $sourceOSDisk.DiskSizeGB -SourceResourceId $sourceOSDisk.Id -CreateOption Copy
+
+#! Create the new disk
+Write-Verbose "Create the new OS disk..."
+$newOSDisk = New-AzDisk -Disk $diskConfig -DiskName $NewDiskName -ResourceGroupName $resourceGroup
---
+++ Enable Azure VM Selective Disk Backup.ps1
@@ -0,0 +1,25 @@
+﻿#enable Selective disk backup
+
+Connect-AzAccount 
+
+Get-AzDisk -ResourceGroupName "ResourceGroupName"
+
+$Vm = Get-AzVM -ResourceGroupName "ResourceGroupName" -Name "Servername"
+
+$vm.StorageProfile.DataDisks
+
+#update the Lun Number
+$disks = (“1”)
+#Set the Target Vault
+$targetVault = Get-AzRecoveryServicesVault -ResourceGroupName "ResourceGroupName Recovery Service Vault" -Name "RecoveryServiceVaultName"
+
+#Set the recovery service vault configuraion
+Set-AzRecoveryServicesVaultContext -Vault $targetVault
+
+#set backup policy
+$pol = Get-AzRecoveryServicesBackupProtectionPolicy -Name “DEV12AM7D”
+
+#Enable backup
+Enable-AzRecoveryServicesBackupProtection -Policy $pol -Name “ServerName” -ResourceGroupName “ResourceGroup” -InclusionDisksList $disks -VaultId $targetVault.ID
+
+$item= Get-AzRecoveryServicesBackupItem -BackupManagementType “AzureVM” -WorkloadType “AzureVM” -VaultId $targetVault.ID -FriendlyName “LinuxVm01”
